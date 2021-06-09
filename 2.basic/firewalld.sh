969da65b7fa23fe10577467875373e742312a976
许忠亮 <geniusmickymouse@qq.com>
2021-06-09 15:34:15 +0800

    refactor

--- 2.basic/firewalld.sh
+++ 2.basic/firewalld.sh
@@ -1,13 +1,15 @@
 systemctl start firewalld.service
 
-systemctl status firewalld.service
-
 systemctl enable firewalld.service
+
+firewall-cmd --state
 
 firewall-cmd --list-ports
 
 firewall-cmd --add-port=80/tcp --permanent
 
 firewall-cmd --add-port=443/tcp --permanent
+
+firewall-cmd --reload
 
 firewall-cmd --list-ports

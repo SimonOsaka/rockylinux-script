c88919f8b09e99eaa9fd0ec9b5bd76078ad743f6
许忠亮 <geniusmickymouse@qq.com>
2021-06-12 15:41:45 +0800

    add podman & nginx script

--- 3.podman/nginx/pull_nginx.sh
+++ 3.podman/nginx/pull_nginx.sh
@@ -2,10 +2,20 @@
 podman pull docker.io/nginx:stable-alpine
 
 mkdir -p /data/nginx/html
+mkdir -p /data/nginx/log
+mkdir -p /data/nginx/conf
+mkdir -p /data/nginx/conf.d
 
 echo "success test!!!" > /data/nginx/html/test.html
 
-podman run -it --name nginx_podman --privileged=true -p 80:80 -v /data/nginx/html:/usr/share/nginx/html -d docker.io/nginx:stable-alpine
+podman run -it --name nginx_podman \
+--privileged=true \
+-p 80:80 \
+-v /data/nginx/html:/usr/share/nginx/html \
+-v /data/nginx/log:/var/log/nginx \
+-v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
+-v /data/nginx/conf.d:/etc/nginx/conf \
+-d docker.io/nginx:stable-alpine
 
 podman ps
 

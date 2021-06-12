5853c0be850783c9a6b72790255e976048636e81
许忠亮 <geniusmickymouse@qq.com>
2021-06-12 13:07:31 +0800

    add podman & nginx script

--- 3.podman/mirror.sh
+++ 3.podman/mirror.sh
@@ -3,7 +3,7 @@ echo "unqualified-search-registries = [\"docker.io\", \"k8s.gcr.io\"]" > /etc/co
 
 echo "[[registry]]" >> /etc/containers/registries.conf
 echo "prefix = \"docker.io\"" >> /etc/containers/registries.conf
-echo "location = \"hub-mirror.c.163.com\"" >> /etc/containers/registries.conf
+echo "location = \"docker.mirrors.ustc.edu.cn\"" >> /etc/containers/registries.conf
 
 echo "[[registry]]" >> /etc/containers/registries.conf
 echo "prefix = \"k8s.gcr.io\"" >> /etc/containers/registries.conf

b5b6174bad7215acb8c52b9b7898eba2e8dfb6f7
许忠亮 <geniusmickymouse@qq.com>
2021-06-13 18:56:21 +0800

    add redis script

--- 3.podman/redis/init_redis.sh
+++ 3.podman/redis/init_redis.sh
@@ -4,8 +4,8 @@ sh pull_redis.sh
 mkdir -p /data/redis/data
 mkdir -p /data/redis/conf
 
-wget http://download.redis.io/redis-stable/redis.conf \
--o /data/redis/conf/redis.conf
+wget -4 http://download.redis.io/redis-stable/redis.conf \
+-O /data/redis/conf/redis.conf
 
 sed -e 's|^bind 127.0.0.1 -::1|#bind 127.0.0.1 -::1|g' \
     -e 's|^protected-mode yes|protected-mode no|g' \

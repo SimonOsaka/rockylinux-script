1fb827a4ec2e36c272ac6548444205e81499fdef
许忠亮 <geniusmickymouse@qq.com>
2021-06-13 17:02:19 +0800

    add postgresql script

--- 3.podman/postgres/init_postgres.sh
+++ 3.podman/postgres/init_postgres.sh
@@ -4,7 +4,9 @@ sh pull_postgres.sh
 mkdir -p /data/postgres/data
 mkdir -p /data/postgres/conf
 
-podman run -i --rm --name postgres_podman cat /usr/share/postgresql/postgresql.conf.sample > /data/postgres/conf/postgres.conf
+podman run -itd -e POSTGRES_PASSWORD=123456 --name postgres_podman docker.io/postgres:alpine
+
+podman cp postgres_podman:/usr/local/share/postgresql/postgresql.conf.sample /data/postgres/conf/postgres.conf
 
 sh rm_postgres.sh
 

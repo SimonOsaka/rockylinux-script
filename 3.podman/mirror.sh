4aaf4234939467e83a3e6fa9ad52adcb27a5462e
Simon Osaka <geniusmickymouse@qq.com>
2021-06-16 10:51:18 +0000

    add quay.io mirror

--- 3.podman/mirror.sh
+++ 3.podman/mirror.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "unqualified-search-registries = [\"docker.io\", \"k8s.gcr.io\"]" > /etc/containers/registries.conf
+echo "unqualified-search-registries = [\"docker.io\", \"quay.io\", \"k8s.gcr.io\"]" > /etc/containers/registries.conf
 
 echo "[[registry]]" >> /etc/containers/registries.conf
 echo "prefix = \"docker.io\"" >> /etc/containers/registries.conf
 echo "location = \"docker.mirrors.ustc.edu.cn\"" >> /etc/containers/registries.conf
+
+echo "[[registry]]" >> /etc/containers/registries.conf
+echo "prefix = \"quay.io\"" >> /etc/containers/registries.conf
+echo "location = \"quay.mirrors.ustc.edu.cn\"" >> /etc/containers/registries.conf
 
 echo "[[registry]]" >> /etc/containers/registries.conf
 echo "prefix = \"k8s.gcr.io\"" >> /etc/containers/registries.conf

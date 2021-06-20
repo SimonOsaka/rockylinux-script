cc32cacaf670fdf211a83f967e514476ec05c817
Simon Osaka <geniusmickymouse@qq.com>
2021-06-20 06:18:31 +0000

    add drone script

--- 3.podman/drone/init_drone.sh
+++ 3.podman/drone/init_drone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /data/drone/data
-
+mkdir -p /data/drone/docker
 sh run_drone.sh
 
 sh show_drone.sh
--- 3.podman/drone/rm_drone.sh
+++ 3.podman/drone/rm_drone.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+podman exec drone_server_podman_docker_1 docker stop runner_docker
+
+podman exec drone_server_podman_docker_1 docker rm runner_docker
+
 podman-compose -p drone_server_podman down
